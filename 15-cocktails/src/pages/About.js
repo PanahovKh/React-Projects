52ce5c6e4c581d527a2dd3765074f0b76f1daffc
panahovkh <xanali.panahov1994@gmail.com>
2021-02-11 17:22:13 +0400

    adding Cocktails proj

---
+++ 15-cocktails/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ea, quae dialectici nunc tradunt et docent, nonne ab illis instituta sunt aut inventa sunt? Idcirco enim non desideraret, quia, quod dolore caret, id in voluptate est. Optime, inquam. Non enim, si omnia non sequebatur, idcirco non erat ortus illinc. Duo Reges: constructio interrete. Hoc ipsum elegantius poni meliusque potuit. Quid enim tanto opus est instrumento in optimis artibus comparandis? Nam et complectitur verbis, quod vult, et dicit plane, quod intellegam; Sed potestne rerum maior esse dissensio? 
+      </p>
+    </section>
+  )
+}
+
+export default About
